a26c4edc09183af0358f618c3c9b4341a10e7135
ldj <lidongjie2000@163.com>
2024-12-15 16:06:06 +0800

    sql_pool代码优化，日志信息添加

--- src/mysqlPool/sql_connection_pool.cpp
+++ src/mysqlPool/sql_connection_pool.cpp
@@ -1,17 +1,13 @@
-//
-// Created by Admin on 2024/12/9.
-//
 #include "../include/mysqlPool/sql_connection_pool.h"
 #include "../include/Log/log.h"
-ConnectionPool::ConnectionPool():m_CurConn(0), m_FreeConn(0) {
 
+ConnectionPool::ConnectionPool():m_CurConn(0), m_FreeConn(0) {
 
 }
 
 ConnectionPool::~ConnectionPool() {
     destroyPool();
 }
-
 
 void ConnectionPool::init(const std::string &url, const std::string &User, const std::string &PassWord,
                           const std::string &DataBaseName, int Port, int MaxConn, int close_log) {
@@ -22,11 +18,11 @@ void ConnectionPool::init(const std::string &url, const std::string &User, const
     m_Port = Port;
     m_close_log = close_log;
 
-    //初始化数据库连接池
-    for(int i=0; i< MaxConn; i++){
+    // 初始化数据库连接池
+    for (int i = 0; i < MaxConn; i++) {
         MYSQL *con = mysql_init(nullptr);
-        if(!con){
-            LOG_ERROR("MySQL Init Error")
+        if (!con) {
+            LOG_ERROR("MySQL Init Error");
             throw std::exception();
         }
         con = mysql_real_connect(con,
@@ -37,9 +33,9 @@ void ConnectionPool::init(const std::string &url, const std::string &User, const
                                  Port,
                                  nullptr,
                                  0);
-        if(!con)
-        {
+        if (!con) {
             LOG_ERROR("MySQL Connect Error: %s", mysql_error(con));
+            destroyPool(); // 清理已创建的资源
             throw std::exception();
         }
 
@@ -50,47 +46,56 @@ void ConnectionPool::init(const std::string &url, const std::string &User, const
     LOG_INFO("Connection Pool Initialized. Max Connections: %d", m_MaxConn);
 }
 
-//单例模式，数据库连接池
+// 单例模式，数据库连接池
 ConnectionPool *ConnectionPool::getInstance() {
     static ConnectionPool connPool;
     return &connPool;
 }
 
-//获取数据库连接
+// 获取数据库连接
 MYSQL *ConnectionPool::getConnection() {
     std::unique_lock<std::mutex> lock(m_mutex);
-    //TODO 检查这里的逻辑是否有点问题
-    while (m_connList.empty())
-    {
+    while (m_connList.empty()) {
         m_cond.wait(lock);
     }
+
+    if (m_connList.empty()) {
+        LOG_ERROR("No available connections in the pool!");
+        return nullptr;
+    }
+
     MYSQL *con = m_connList.front();
     m_connList.pop_front();
     m_FreeConn--;
     m_CurConn++;
+    LOG_INFO("Connection acquired. Free: %d, Current: %d", m_FreeConn, m_CurConn);
     return con;
 }
 
 bool ConnectionPool::releaseConnection(MYSQL *conn) {
-    if(!conn) return false;
+    if (!conn) return false;
     std::unique_lock<std::mutex> lock(m_mutex);
+    if (m_CurConn <= 0) {
+        LOG_ERROR("Invalid connection release attempt!");
+        return false;
+    }
     m_connList.push_back(conn);
     m_FreeConn++;
     m_CurConn--;
-    m_cond.notify_one();//有空闲的资源，唤醒等待线程
+    m_cond.notify_one(); // 有空闲的资源，唤醒等待线程
+    LOG_INFO("Connection released. Free: %d, Current: %d", m_FreeConn, m_CurConn);
     return true;
-
-
 }
 
 void ConnectionPool::destroyPool() {
     std::unique_lock<std::mutex> lock(m_mutex);
-    for(auto conn:m_connList){
+    for (auto conn : m_connList) {
         mysql_close(conn);
     }
     m_connList.clear();
     m_FreeConn = 0;
     m_CurConn = 0;
+    LOG_INFO("Connection pool destroyed.");
 }
 
 int ConnectionPool::getFreeConn() {
@@ -98,9 +103,13 @@ int ConnectionPool::getFreeConn() {
     return m_FreeConn;
 }
 
-//con 可以存储一个 MYSQL* 类型的指针的地址
+// con 可以存储一个 MYSQL* 类型的指针的地址
 ConnectionRAII::ConnectionRAII(MYSQL **con, ConnectionPool *connPool) {
     *con = connPool->getConnection();
+    if (*con == nullptr) {
+        LOG_ERROR("Failed to acquire connection from pool!");
+        throw std::runtime_error("No available connections");
+    }
     conRAII = *con;
     poolRAII = connPool;
 }
